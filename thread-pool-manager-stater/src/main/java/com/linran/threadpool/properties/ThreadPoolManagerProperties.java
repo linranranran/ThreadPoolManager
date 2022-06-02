6556d8c658d262c4ee9c4b54c59d56b041a790ee
林志达 <386126949@qq.com>
2022-06-02 17:13:46 +0800

    开始构建配置

--- thread-pool-manager-stater/src/main/java/com/linran/threadpool/properties/ThreadPoolManagerProperties.java
+++ thread-pool-manager-stater/src/main/java/com/linran/threadpool/properties/ThreadPoolManagerProperties.java
@@ -3,9 +3,44 @@ package com.linran.threadpool.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
+ * linran:
+ *
+ *  --设置默认线程池name，如果手动设置了则默认线程池名称为该设置，但是参数仍可通过“default:”来设置。
+ *  default-name:xxxx
+ *
+ *  -- 全局设置，即默认值，未手动设置的参数的实例优先使用该设置。
+ *  global-set:
+ *
+ *  -- default线程池是默认的，设不设置都会创建该线程池实例。
+ *  default:
+ *
+ *      -- true表示开启自动设置，如果开启，其它没有手动设置的参数均为默认设置，手动设置优先级更高。
+ *      auto-set:true/false
+ *
+ *      main-size:
+ *      max-size:
+ *      alive-time:
+ *      time-util:
+ *      queue-type:
+ *      queue-size:
+ *      thread-factory:
+ *      reject-handler:
+ *
+ *  -- 申明"xxx"线程池
+ *  xxxx:
+ *      ....
+ *      ....
+ *
+ *  -- 申明"aaa"和"bbb"两个线程池，并且共用同一个参数设置。
+ *  aaa,bbb:
+ *      ....
+ *      ....
  * @Author LinRan
  * @Date 2022/6/1
  */
 @ConfigurationProperties(prefix = "linran")
 public class ThreadPoolManagerProperties {
+
+
+
 }
