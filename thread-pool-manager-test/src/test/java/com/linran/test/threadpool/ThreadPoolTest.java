274a4c7c1015eb7a4da0385bd74797242955dbaa
林志达 <386126949@qq.com>
2022-07-04 17:07:27 +0800

    示例更新

--- thread-pool-manager-test/src/test/java/com/linran/test/threadpool/ThreadPoolTest.java
+++ thread-pool-manager-test/src/test/java/com/linran/test/threadpool/ThreadPoolTest.java
@@ -1,5 +1,10 @@
 package com.linran.test.threadpool;
 
+import com.linran.threadpool.constant.ThreadPoolAddType;
+import com.linran.threadpool.factory.pool.DefaultThreadPoolFactory;
+import com.linran.threadpool.factory.pool.ThreadPoolFactory;
+import com.linran.threadpool.handler.ThreadPoolHolder;
+import com.linran.threadpool.interceptor.DefaultThreadPoolInterceptor;
 import com.linran.threadpool.task.AbstractRunnable;
 
 import com.linran.threadpool.util.ThreadPoolManager;
@@ -10,6 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @Author LinRan
@@ -25,6 +32,30 @@ public class ThreadPoolTest {
     @Test
     public void test1(){
 
+        threadPoolManager.addPoolInstance("order-pool" , new DefaultThreadPoolFactory("order-pool"));
+        threadPoolManager.addPoolInstance("order-pool" , new DefaultThreadPoolFactory("order-pool").createBasicThreadPoolInstance());
+        threadPoolManager.addPoolInstance("order-pool" , new DefaultThreadPoolFactory("order-pool").createBasicThreadPoolInstance() , ThreadPoolAddType.ABANDON_IF_EXIST);
+
+//        AbstractRunnable runnable = new AbstractRunnable() {
+//            @Override
+//            public void before() {
+//                //do something before
+//            }
+//
+//            @Override
+//            public void after() {
+//                //do something after
+//            }
+//
+//            @Override
+//            public void task() {
+//                //do job,just like run()
+//            }
+//        };
+//        long runnableNum = threadPoolManager.execute(runnable);
+
+        threadPoolManager.addPoolInterceptor(new DefaultThreadPoolInterceptor());
+        threadPoolManager.addPoolInterceptor("pool-name",new DefaultThreadPoolInterceptor());
         for(int i = 0 ; i < 10 ; i++){
             AbstractRunnable runnable = new AbstractRunnable() {
                 /**
